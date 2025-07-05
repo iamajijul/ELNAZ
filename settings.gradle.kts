d1ddbd8712aec097036400c1924acbdff7080de0
Ajijul Mondal <ajijulmondal786@gmail.com>
2025-07-06 02:32:39 +0400

    Add - All Modules and themes related works

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ELNAZ"
+include(":app")
+include(":data")
+include(":domain")
+include(":ui")
+include(":feature-barcode")
+include(":feature-additem")
---
+++ ui/src/main/java/com/ajijul/elnaz/ui/theme/AppTextSizes.kt
@@ -0,0 +1,10 @@
+package com.ajijul.elnaz.ui.theme
+
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+object AppTextSizes {
+    val BodyLarge: TextUnit = 16.sp
+    val TitleLarge: TextUnit = 22.sp
+    val LabelSmall: TextUnit = 12.sp
+}
