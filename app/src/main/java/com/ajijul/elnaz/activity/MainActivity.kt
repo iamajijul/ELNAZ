31537aae69db543d29d0aec5d2f4b90227191f8d
Ajijul Mondal <ajijulmondal786@gmail.com>
2025-07-20 02:34:50 +0400

    Add - Feature module install on user action and handle navigation.

--- app/src/main/java/com/ajijul/elnaz/activity/MainActivity.kt
+++ app/src/main/java/com/ajijul/elnaz/activity/MainActivity.kt
@@ -5,14 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.ajijul.elnaz.components.AppNavigation
 import com.ajijul.elnaz.core.ui.theme.ElnazInventoryTheme
+import com.ajijul.elnaz.startup.FeatureModuleInstaller
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    //region VARIABLES
+
+    @Inject
+    lateinit var featureModuleInstaller: FeatureModuleInstaller
+
+    //endregion
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ElnazInventoryTheme(content = { AppNavigation() })
+            ElnazInventoryTheme(content = { AppNavigation(featureModuleInstaller) })
         }
     }
 }
--- app/src/main/java/com/ajijul/elnaz/components/AppNavigations.kt
+++ app/src/main/java/com/ajijul/elnaz/components/AppNavigations.kt
@@ -5,42 +5,40 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.ajijul.elnaz.core.utils.NavRoutes
+import com.ajijul.elnaz.extension.TAG
 import com.ajijul.elnaz.inventory.InventoryScreen
+import com.ajijul.elnaz.logger.ElnazLogger
+import com.ajijul.elnaz.startup.FeatureModuleInstaller
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.google.android.play.core.splitinstall.SplitInstallRequest
 
 @Composable
-fun AppNavigation() {
+fun AppNavigation(featureModuleInstaller: FeatureModuleInstaller) {
     val navController = rememberNavController()
-    val splitInstallManager = SplitInstallManagerFactory.create(navController.context)
+    NavHost(navController = navController, startDestination = NavRoutes.INVENTORY) {
 
-    NavHost(navController = navController, startDestination = NavRoutes.INVENTORY){
-
-        composable(NavRoutes.INVENTORY){
+        composable(NavRoutes.INVENTORY) {
             InventoryScreen(
                 onNavigateToAddItem = {
-                    val request = SplitInstallRequest.newBuilder()
-                        .addModule(NavRoutes.FEATURE_ADD_ITEM)
-                        .build()
-                    splitInstallManager.startInstall(request)
-                        .addOnSuccessListener { navController.navigate(NavRoutes.ADD_ITEM) }
-                        .addOnFailureListener { /* Handle failure */ }
+                    featureModuleInstaller.installModule(NavRoutes.FEATURE_ADD_ITEM) {
+                        navController.navigate(NavRoutes.ADD_ITEM)
+                    }
                 },
                 onNavigateToBarcodeScanner = {
-                    val request = SplitInstallRequest.newBuilder()
-                        .addModule(NavRoutes.FEATURE_BARCODE)
-                        .build()
-                    splitInstallManager.startInstall(request)
-                        .addOnSuccessListener { navController.navigate(NavRoutes.BARCODE_SCANNER) }
-                        .addOnFailureListener { /* Handle failure */ }
+                    featureModuleInstaller.installModule(NavRoutes.FEATURE_BARCODE) {
+                        navController.navigate(NavRoutes.BARCODE_SCANNER)
+                    }
                 }
             )
-            composable(NavRoutes.ADD_ITEM) {
-                // Dynamically loaded by :additem
-            }
-            composable(NavRoutes.BARCODE_SCANNER) {
-                // Dynamically loaded by :barcode
-            }
         }
+
+        composable(NavRoutes.ADD_ITEM) {
+            // Dynamically loaded by :additem
+        }
+
+        composable(NavRoutes.BARCODE_SCANNER) {
+            // Dynamically loaded by :barcode
+        }
+
     }
 }
