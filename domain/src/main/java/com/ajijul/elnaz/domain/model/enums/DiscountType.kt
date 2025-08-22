7fa5457e6b47afe9913d06ea5955c2d1b4e92010
Ajijul Mondal <ajijulmondal786@gmail.com>
2025-08-22 22:40:05 +0400

    Add - Added feature module

---
+++ domain/src/main/java/com/ajijul/elnaz/domain/model/enums/DiscountType.kt
@@ -0,0 +1,6 @@
+package com.ajijul.elnaz.domain.model.enums
+
+enum class DiscountType {
+    PERCENTAGE,
+    FLAT
+}
---
+++ domain/src/main/java/com/ajijul/elnaz/domain/model/enums/UserRole.kt
@@ -0,0 +1,7 @@
+package com.ajijul.elnaz.domain.model.enums
+
+enum class UserRole {
+    STAFF,
+    ADMIN,
+    SUPER_ADMIN
+}
